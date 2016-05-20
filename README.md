d3f9d004a9496e52d3e201c9fdecaf3cf6dce10a
Stoyko Stanchev <stoyko.stanchev@flyvictor.com>
2016-05-20 17:09:37 +0100

    Temp commit, browserSync halfway

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 gulp compile_scss to compile and put the css into the dist/styles/css folder
 gulp watch - watches the .scss files and runs their compilation task
+https://css-tricks.com/gulp-for-beginners/
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require( "gulp" );
 var sass = require( "gulp-sass" );
+var browserSync = require('browser-sync').create();
 
 gulp.task('compile_scss', function(){
   return gulp.src('app/common/styles/scss/**/*.scss')
       .pipe(sass()) // Converts Sass to CSS with gulp-sass
       .pipe(gulp.dest('dist/styles/css'))
+      .pipe(browserSync.reload({
+          stream: true
+       }));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'compile_scss'], function(){
   gulp.watch('app/common/styles/scss/**/*.scss', [ 'compile_scss' ]);
+});
+
+gulp.task( 'browserSync', function() {
+  browserSync.init({
+    server : {
+      baseDir : 'dist'
+    }
+  });
 });
