ac50d181abf4c61611196df10c85f934334c5fcc
Stoyko Stanchev <stoyko.stanchev@flyvictor.com>
2016-05-20 13:52:46 +0100

    Added gulp_watch - watches .scss files and compiles them

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 gulp compile_scss to compile and put the css into the dist/styles/css folder
+gulp watch - watches the .scss files and runs their compilation task
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require( "gulp" );
 var sass = require( "gulp-sass" );
 
 gulp.task('compile_scss', function(){
-  return gulp.src('app/common/styles/scss/styles.scss')
+  return gulp.src('app/common/styles/scss/**/*.scss')
       .pipe(sass()) // Converts Sass to CSS with gulp-sass
       .pipe(gulp.dest('dist/styles/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/common/styles/scss/**/*.scss', [ 'compile_scss' ]);
 });
