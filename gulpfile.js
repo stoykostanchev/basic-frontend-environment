3c335d81b7ebee6a56debdb4d85107bf4fbfab8a
Stoyko Stanchev <stoykostanchev@flyvictor.com>
2016-05-23 08:59:26 +0100

    Adds an index and copying of html files to the dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require( "gulp" );
 var sass = require( "gulp-sass" );
 var browserSync = require('browser-sync').create();
 
+gulp.task('compile_html', function(){
+  return gulp.src('app/**/*.html')
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.reload({
+          stream: true
+       }));
+});
+
 gulp.task('compile_scss', function(){
   return gulp.src('app/common/styles/scss/**/*.scss')
       .pipe(sass()) // Converts Sass to CSS with gulp-sass
@@ -11,8 +19,9 @@ gulp.task('compile_scss', function(){
        }));
 });
 
-gulp.task('watch', ['browserSync', 'compile_scss'], function(){
+gulp.task('watch', ['browserSync', 'compile_scss', 'compile_html'], function(){
   gulp.watch('app/common/styles/scss/**/*.scss', [ 'compile_scss' ]);
+  gulp.watch('app/**/*.html', [ 'compile_html' ]);
 });
 
 gulp.task( 'browserSync', function() {
